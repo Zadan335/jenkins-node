c91f9868f718f4ed43a4dd983d65a2281d0b78b0
Ubuntu <VMADMIN@LU-Zain.1y4kkons3omuhdjn15nkkiwndb.xx.internal.cloudapp.net>
2025-12-01 20:25:28 +0000

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req,res)=>{
+  res.end(`Hello from ${process.env.ENV} environment!`);
+});
+server.listen(port,()=>console.log(`App running on ${port}`));
